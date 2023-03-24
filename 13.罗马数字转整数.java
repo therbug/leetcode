0dc5e7df5f7092399e9e774b22a64ec2e2b08aa7
TherBug <cuitao2000@126.com>
2023-03-24 20:26:51 +0800

    13

---
+++ 13.罗马数字转整数.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * @lc app=leetcode.cn id=13 lang=java
+ *
+ * [13] 罗马数字转整数
+ */
+
+// @lc code=start
+class Solution {
+
+    Map<Character, Integer> symbolValues = new HashMap<Character, Integer>() {{
+        put('I', 1);
+        put('V', 5);
+        put('X', 10);
+        put('L', 50);
+        put('C', 100);
+        put('D', 500);
+        put('M', 1000);
+    }};
+
+    public int romanToInt(String s) {
+        int res = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int value = symbolValues.get(s.charAt(i));
+            if (i < s.length() - 1 && value < symbolValues.get(s.charAt(i + 1))) {
+                res -= value;
+            } else {
+                res += value;
+            }
+        }
+        return res;
+    }
+}
+// @lc code=end
+
