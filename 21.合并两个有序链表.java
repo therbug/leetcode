8117582831349040c0e5526265d35d042167ba14
TherBug <cuitao2000@126.com>
2023-03-25 12:36:46 +0800

    关闭爆红

--- 21.合并两个有序链表.java
+++ 21.合并两个有序链表.java
@@ -51,16 +51,25 @@
  */
 
 // @lc code=start
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+
+// Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         ListNode dummy = new ListNode(-1), p = dummy;
@@ -85,4 +94,3 @@ class Solution {
     }
 }
 // @lc code=end
-
