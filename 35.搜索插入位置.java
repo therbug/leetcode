01d18f77318d9441c3e14bfb21a7252ac5567a87
ther <cuitao2000@126.com>
2023-06-20 14:52:31 +0800

    35,58,66

---
+++ 35.搜索插入位置.java
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=35 lang=java
+ *
+ * [35] 搜索插入位置
+ */
+
+// @lc code=start
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        if (nums.length == 0)
+            return -1;
+        int left = 0;
+        int right = nums.length;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                right = mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid;
+            }
+        }
+        return left;
+    }
+}
+// @lc code=end
